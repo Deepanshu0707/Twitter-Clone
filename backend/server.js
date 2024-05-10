08346f2f8395f552fc2149574b278e6644f38541
Deepanshu <deepanshubisht00@gmail.com>
2024-05-10 13:33:32 +0530

    Change In PORT VALUE

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use("/api/auth",authRoutes);
 
 
-app.listen(8000,()=>{
+app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
     connectMongoDB();
 })
