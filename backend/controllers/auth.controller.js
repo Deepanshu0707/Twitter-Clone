bab1c5ab02827d70090c72dc049d6b154502571c
Deepanshu <deepanshubisht00@gmail.com>
2024-05-10 13:29:15 +0530

    1- Project Setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,19 @@
+export const signup = (req,res)=>{
+    res.json({
+        data: "You hit the singup endpoint",
+    })
+}
+
+
+export const login = (req,res)=>{
+    res.json({
+        data: "You hit the login endpoint",
+    })
+}
+
+
+export const logout = (req,res)=>{
+    res.json({
+        data: "You hit the logout endpoint",
+    })
+}
