31270183b01ec118690450121fa796b96e72035b
Deepanshu <deepanshubisht00@gmail.com>
2024-05-15 09:34:15 +0530

    Added Signup,Login,Logout,getMe whenever a user already login it will check getMe to identify that the right user login in or not.

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,16 @@ import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+app.use(express.json()); //For parsing req.body data//
+app.use(express.urlencoded({extended:true})); //to parse form data
+app.use(cookieParser()); //for accessing cookies of client
 app.use("/api/auth",authRoutes);
 
 
