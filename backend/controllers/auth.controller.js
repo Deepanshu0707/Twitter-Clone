71e0c9d8708fa1ba05db4e3dbfed992afd7c1704
Deepanshu <deepanshubisht00@gmail.com>
2024-05-16 11:46:45 +0530

    1. User Routes and controllers added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -38,7 +38,7 @@ export const signup = async(req,res)=>{
             generateTokenAndSetCookie(newUser._id,res)
             await newUser.save();
 
-            res.status(201).json({
+           return res.status(201).json({
                 _id: newUser._id,
                 fullName: newUser.fullName,
                 username: newUser.username,
@@ -49,12 +49,12 @@ export const signup = async(req,res)=>{
                 coverImg: newUser.coverImg,
             })
         } else{
-            res.status(400).json({error:"Invalid user data"});
+          return res.status(400).json({error:"Invalid user data"});
         }
 
     } catch (error) {
         console.log("Error in signup controller", error.message);
-        res.status(500).json({error:"Internal Server Error"});
+      return res.status(500).json({error:"Internal Server Error"});
     }
 }
 
@@ -71,7 +71,7 @@ export const login = async(req,res)=>{
 
     generateTokenAndSetCookie(user._id,res);
 
-    res.status(200).json({
+   return res.status(200).json({
         _id: user._id,
         fullName: user.fullName,
         username: user.username,
@@ -84,7 +84,7 @@ export const login = async(req,res)=>{
 
    } catch (error) {
     console.log("Error in login controller", error.message);
-    res.status(500).json({error:"Internal Server Error"});
+   return res.status(500).json({error:"Internal Server Error"});
    }
 }
 
@@ -92,10 +92,10 @@ export const login = async(req,res)=>{
 export const logout = async(req,res)=>{
     try {
         res.cookie("jwt","",{maxAge:0})
-        res.status(200).json({message:"Logged out successfully"});
+      return res.status(200).json({message:"Logged out successfully"});
     } catch (error) {
         console.log("Error in logout controller", error.message);
-        res.status(500).json({error:"Internal Server Error"});
+       return res.status(500).json({error:"Internal Server Error"});
     }
 }
 
@@ -103,9 +103,9 @@ export const logout = async(req,res)=>{
 export const getMe = async(req,res)=>{
     try {
         const user = await User.findById(req.user._id).select("-password");
-        res.status(200).json(user);
+       return res.status(200).json(user);
     } catch (error) {
         console.log("Error in getMe controller", error.message);
-        res.status(500).json({error:"Internal Server Error"});
+       return res.status(500).json({error:"Internal Server Error"});
     }
 }
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
+import {v2 as cloudinary} from "cloudinary"
+
 
 dotenv.config();
+
+cloudinary.config({
+    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+    api_key : process.env.CLOUDINARY_API_KEY,
+    api_secret : process.env.CLOUDINARY_API_SECRET,
+})
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,8 +21,10 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); //For parsing req.body data//
 app.use(express.urlencoded({extended:true})); //to parse form data
 app.use(cookieParser()); //for accessing cookies of client
-app.use("/api/auth",authRoutes);
 
+
+app.use("/api/auth",authRoutes);
+app.use("/api/users",userRoutes);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
